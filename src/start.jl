c6cc14fa1220777f2626fd53c58a0155092b2be2
Bernhard König <github@diro.ch>
2024-01-25 14:27:30 +0100

    ENV["GKSwstype"]="nul"

--- src/start.jl
+++ src/start.jl
@@ -6,4 +6,7 @@ Pkg.instantiate()
 using DockerPlotsMWE
 using Dates
 
+#deactivate display #https://discourse.julialang.org/t/deactivate-plot-display-to-avoid-need-for-x-server/19359/13 #https://discourse.julialang.org/t/plots-jl-in-a-docker-container/109254/2
+ENV["GKSwstype"]="nul"
+
 main()
